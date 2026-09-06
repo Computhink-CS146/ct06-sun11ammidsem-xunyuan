2c69c937ba67c011abd45a405eecc981d8e44de4
Computhink-CS146 <cs146@computhink.com.sg>
2026-09-06 03:51:47 +0000

    Sep 6, 2026, 3:51 AM

--- testquestion1.js
+++ testquestion1.js
@@ -10,6 +10,6 @@ function draw(){
     fill("black");
     stroke("red");
     strokeWeight(10);
-    triangle(300,150,150,150,90,110);
+    triangle(300,150,150,200,90,110);
     rect(300,50,90,90)
 }
